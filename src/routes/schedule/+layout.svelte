d168ebe40878a2b5d5eaa400fdda4b9007483b5a
Penbu <penbu@penbuvt.ca>
2024-01-25 21:23:07 -0500

    Schedule: Add schedule skeleton

---
+++ src/routes/schedule/+layout.svelte
@@ -0,0 +1,18 @@
+<slot />
+
+<style lang="scss">
+  // Global border-box
+  :global(html) {
+    box-sizing: border-box;
+  }
+  :global(*, *:before, *:after) {
+    box-sizing: inherit;
+  }
+
+  :global(html),
+  :global(body) {
+    margin: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
