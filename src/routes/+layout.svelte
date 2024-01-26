15b98f8d709129c1e28a98311892fcb355921eff
Penbu <penbu@penbuvt.ca>
2024-01-25 21:06:02 -0500

    Add CSS border-box reset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <slot />
 
 <style lang="scss">
+  // Global border-box
+  :global(html) {
+    box-sizing: border-box;
+  }
+  :global(*, *:before, *:after) {
+    box-sizing: inherit;
+  }
+
   :global(body) {
     margin: 0;
   }
