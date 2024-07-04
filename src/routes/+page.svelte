1acfd1002e0a06ebd1a25772ec796480c566199f
Penbu <penbu@penbuvt.ca>
2024-07-03 20:23:57 -0400

    Add social icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
+<script>
+  import youtubeIcon from '$lib/images/social-icons/youtube.svg';
+  import youtubeIconColor from '$lib/images/social-icons/youtube-color.svg';
+  import twitchIcon from '$lib/images/social-icons/twitch.svg';
+  import twitchIconColor from '$lib/images/social-icons/twitch-color.svg';
+  import discordIcon from '$lib/images/social-icons/discord.svg';
+  import discordIconColor from '$lib/images/social-icons/discord-color.svg';
+  import xIcon from '$lib/images/social-icons/x.svg';
+  import xIconColor from '$lib/images/social-icons/x-color.svg';
+  import githubIcon from '$lib/images/social-icons/github.svg';
+  import githubIconColor from '$lib/images/social-icons/github-color.svg';
+</script>
+
 <div>
   <main>
     <div class="hero">
@@ -8,6 +21,23 @@
         <h2 class="subtitle">
           Programming, puzzle-gaming, music-making vtuber on Linux.
         </h2>
+        <div class="socials">
+          <a class="social-icon" href="https://www.youtube.com/@PenbuVT" style="--color-src: url({youtubeIconColor});" title="YouTube">
+            <img width="64" height="64" src="{youtubeIcon}" alt="YouTube" />
+          </a>
+          <a class="social-icon" href="https://twitch.tv/penbuvt" style="--color-src: url({twitchIconColor});" title="Twitch">
+            <img width="64" height="64" src="{twitchIcon}" alt="Twitch" />
+          </a>
+          <a class="social-icon" href="https://discord.gg/X5XZDfBSPG" style="--color-src: url({discordIconColor});" title="Discord">
+            <img width="64" height="64" src="{discordIcon}" alt="Discord" />
+          </a>
+          <a class="social-icon" href="https://twitter.com/penbuvt" style="--color-src: url({xIconColor});" title="X (Twitter)">
+            <img width="64" height="64" src="{xIcon}" alt="X (Twitter)" />
+          </a>
+          <a class="social-icon" href="https://github.com/penbuvt" style="--color-src: url({githubIconColor});" title="GitHub">
+            <img width="64" height="64" src="{githubIcon}" alt="GitHub" />
+          </a>
+        </div>
       </section>
       <section>
         <iframe class="featured-video" width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=PXGk0Hhs4F723Dn2&amp;list=UUPV8mWVjn4ZXjlb3DVnL-bysQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -47,6 +77,46 @@
     box-sizing: border-box;
     padding: 0 2rem;
     gap: 2em;
+  }
+
+  .socials {
+    display: flex;
+    justify-content: center;
+    gap: 1em;
+    flex-wrap: wrap;
+
+    .social-icon {
+      position: relative;
+
+      img {
+        width: 4em;
+        height: 4em;
+
+        opacity: 0.75;
+        transition: opacity 150ms ease-in-out;
+      }
+
+      &::before {
+        content: var(--color-src);
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 4em;
+        height: 4em;
+        z-index: 1;
+
+        opacity: 0;
+        transition: opacity 150ms ease-in-out;
+      }
+
+      &:hover,
+      &:focus {
+        &::before {
+          opacity: 1;
+        }
+      }
+    }
   }
 
   footer {
