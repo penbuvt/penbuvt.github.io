ad32b9d7406bc0f58f30285b3b10f49865398ba4
Penbu <penbu@penbuvt.ca>
2024-07-03 21:39:29 -0400

    Remove redundant styles from schedule layout

--- src/routes/schedule/+layout.svelte
+++ src/routes/schedule/+layout.svelte
@@ -1,14 +1,6 @@
 <slot />
 
 <style lang="scss">
-  // Global border-box
-  :global(html) {
-    box-sizing: border-box;
-  }
-  :global(*, *:before, *:after) {
-    box-sizing: inherit;
-  }
-
   :global(html),
   :global(body) {
     margin: 0;
