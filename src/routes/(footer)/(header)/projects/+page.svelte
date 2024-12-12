34569b650824c7f6764e84429912c0594fbdb998
Penbu <penbu@penbuvt.ca>
2024-12-11 20:06:44 -0500

    Migrate "Projects" index page

---
+++ src/routes/(footer)/(header)/projects/+page.svelte
@@ -0,0 +1,21 @@
+<script type="ts">
+  import { resolveRoute } from '$app/paths';
+  import type { Link } from '$lib/link.ts';
+
+  const pages: Link[] = [
+    { text: 'To You (Original composition)', href: resolveRoute('/projects/to-you/') },
+    { text: 'Make It Through (Original composition)', href: resolveRoute('/projects/make-it-through/') },
+  ];
+</script>
+
+<h1 class="heading-text">Projects</h1>
+
+<p>A list of my released works.</p>
+
+<h2 id="pages">Pages</h2>
+
+<ul>
+  {#each pages as { text, href } (href)}
+    <li><a {href}>{text}</a></li>
+  {/each}
+</ul>
