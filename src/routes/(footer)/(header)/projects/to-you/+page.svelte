d3d81e5c2096f562dc06ee948787402e5179f640
Penbu <penbu@penbuvt.ca>
2024-12-11 19:36:07 -0500

    Migrate "To You" page

---
+++ src/routes/(footer)/(header)/projects/to-you/+page.svelte
@@ -0,0 +1,81 @@
+<h1>To You (Original composition)</h1>
+<p>Released: February 7, 2024</p>
+
+<p>
+  <a href="http://creativecommons.org/licenses/by/4.0/" target="_blank" rel="license noopener noreferrer">
+    <img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by.svg" alt="Creative Commons Attribution 4.0 International" style="height: 2em; width: auto;">
+  </a>
+</p>
+
+<iframe class="featured-video" src="https://www.youtube.com/embed/G9NMT7NDUVE?si=HL-pwjCWF1Mg-un4" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" height="315" frameborder="0" width="560"></iframe>
+<p>Original composition for piano quintet (piano + string quartet).
+  This is my first original composition as a vtuber. It was inspired by a private piano improv session and developed <a href="https://www.youtube.com/playlist?list=PLNMaWutZ8vfMNFjYZE1dvsyLyUcnfKxWW">live on stream</a>.</p>
+<p>The score was created with <a href="https://musescore.org/">MuseScore 3</a> and rendered with the <a href="https://musescore.org/en/handbook/3/soundfonts-and-sfz-files#All_sounds">MuseScore General SoundFont</a>.</p>
+<h2 id="links">Links</h2>
+<ul>
+  <li><a href="https://www.youtube.com/watch?v=G9NMT7NDUVE">Music video</a></li>
+  <li><a href="https://musescore.com/user/73151548/scores/14285818">MuseScore.com post</a></li>
+  <li><a href="https://www.youtube.com/playlist?list=PLNMaWutZ8vfMNFjYZE1dvsyLyUcnfKxWW">Making-of playlist</a></li>
+</ul>
+
+<h2 id="downloads">Downloads</h2>
+
+<ul>
+  <li>Source:
+    <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You.mscz">MuseScore (MSCZ)</a>
+  </li>
+  <li>Full score:
+    <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You.pdf">PDF (score only)</a>,
+    <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Score_and_Parts.pdf">PDF (score and parts)</a>,
+    <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You.flac">FLAC</a>,
+    <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You.mp3">MP3</a>,
+    <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You.ogg">OGG</a>,
+    <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You.mid">MIDI</a>, <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You.mxl">MusicXML (MXL)</a>
+  </li>
+  <li>Parts
+    <ul>
+      <li>Piano:
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Piano.pdf">PDF</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Piano.flac">FLAC</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Piano.mp3">MP3</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Piano.ogg">OGG</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Piano.mid">MIDI</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Piano.mxl">MusicXML (MXL)</a>
+      </li>
+      <li>Violin 1:
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_1.pdf">PDF</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_1.flac">FLAC</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_1.mp3">MP3</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_1.ogg">OGG</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_1.mid">MIDI</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_1.mxl">MusicXML (MXL)</a>
+      </li>
+      <li>Violin 2:
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_2.pdf">PDF</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_2.flac">FLAC</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_2.mp3">MP3</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_2.ogg">OGG</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_2.mid">MIDI</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violin_2.mxl">MusicXML (MXL)</a>
+      </li>
+      <li>Viola:
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Viola.pdf">PDF</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Viola.flac">FLAC</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Viola.mp3">MP3</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Viola.ogg">OGG</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Viola.mid">MIDI</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Viola.mxl">MusicXML (MXL)</a>
+      </li>
+      <li>Cello:
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violoncello.pdf">PDF</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violoncello.flac">FLAC</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violoncello.mp3">MP3</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violoncello.ogg">OGG</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violoncello.mid">MIDI</a>,
+        <a href="https://files.penbuvt.ca/music/to-you/Penbu%20-%20To%20You-Violoncello.mxl">MusicXML (MXL)</a>
+      </li>
+    </ul>
+  </li>
+</ul>
+
+<p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><small><a property="dct:title" rel="cc:attributionURL" href="https://www.penbuvt.ca/portfolio/to-you/">To You</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://www.penbuvt.ca/">Penbu</a> is licensed under <a href="http://creativecommons.org/licenses/by/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY 4.0<img style="height:1em!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt=""><img style="height:1em!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt=""></a></small></p>
