1a1c625657628229a28e2de8e3f18813542e9c8a
Penbu <penbu@penbuvt.ca>
2024-12-11 19:45:50 -0500

    Migrate "Make It Through" page

---
+++ src/routes/(footer)/(header)/projects/make-it-through/+page.svelte
@@ -0,0 +1,85 @@
+<h1 class="heading-text">Make It Through (Original composition)</h1>
+<p>Released: May 17, 2024</p>
+
+<p>
+  <a href="http://creativecommons.org/licenses/by/4.0/" target="_blank" rel="license noopener noreferrer">
+    <img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by.svg" alt="Creative Commons Attribution 4.0 International" style="height: 2em; width: auto;">
+    </a>
+</p>
+  <iframe class="featured-video" src="https://www.youtube.com/embed/pcKH6YUph_M" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="" height="315" frameborder="0" width="560"></iframe>
+  <p>Original composition for rock band.
+  Dedicated to everyone going through tough times: you’ll Make It Through.
+  It was inspired by a private <a href="https://www.youtube.com/watch?v=mWWiI_5LjxY">piano improv session</a> and developed <a href="https://www.youtube.com/playlist?list=PLNMaWutZ8vfP3iYzWvK737pYHks57zGCz">live on stream</a>.</p>
+<p>The score was created with <a href="https://musescore.org/">MuseScore 3</a> and rendered with the <a href="https://musescore.org/en/handbook/3/soundfonts-and-sfz-files#All_sounds">MuseScore General SoundFont</a>.</p>
+
+<h2 id="links">Links</h2>
+
+<ul>
+  <li><a href="https://www.youtube.com/watch?v=pcKH6YUph_M">Music video</a></li>
+  <li><a href="https://musescore.com/user/73151548/scores/16917589">MuseScore.com post</a></li>
+  <li><a href="https://www.youtube.com/watch?v=mWWiI_5LjxY">Original piano improv session</a></li>
+  <li><a href="https://www.youtube.com/playlist?list=PLNMaWutZ8vfP3iYzWvK737pYHks57zGCz">Making-of playlist</a></li>
+</ul>
+
+<h2 id="downloads">Downloads</h2>
+
+<ul>
+  <li>Source:
+    <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through.mscz">MuseScore (MSCZ)</a>
+  </li>
+  <li>Full score:
+    <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through.pdf">PDF (score only)</a>,
+    <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Score_and_Parts.pdf">PDF (score and parts)</a>,
+    <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through.flac">FLAC</a>,
+    <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through.mp3">MP3</a>,
+    <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through.ogg">OGG</a>,
+    <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through.mid">MIDI</a>,
+    <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through.mxl">MusicXML (MXL)</a>
+  </li>
+  <li>Parts
+    <ul>
+      <li>Electric Piano:
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Electric_Piano.pdf">PDF</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Electric_Piano.flac">FLAC</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Electric_Piano.mp3">MP3</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Electric_Piano.ogg">OGG</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Electric_Piano.mid">MIDI</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Electric_Piano.mxl">MusicXML (MXL)</a>
+      </li>
+      <li>Lead Guitar:
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Lead_Guitar.pdf">PDF</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Lead_Guitar.flac">FLAC</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Lead_Guitar.mp3">MP3</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Lead_Guitar.ogg">OGG</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Lead_Guitar.mid">MIDI</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Lead_Guitar.mxl">MusicXML (MXL)</a>
+      </li>
+      <li>Rhythm Guitar:
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Rhythm_Guitar.pdf">PDF</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Rhythm_Guitar.flac">FLAC</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Rhythm_Guitar.mp3">MP3</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Rhythm_Guitar.ogg">OGG</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Rhythm_Guitar.mid">MIDI</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Rhythm_Guitar.mxl">MusicXML (MXL)</a>
+      </li>
+      <li>Bass Guitar:
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Bass_Guitar.pdf">PDF</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Bass_Guitar.flac">FLAC</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Bass_Guitar.mp3">MP3</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Bass_Guitar.ogg">OGG</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Bass_Guitar.mid">MIDI</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Bass_Guitar.mxl">MusicXML (MXL)</a>
+      </li>
+      <li>Drumset:
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Drumset.pdf">PDF</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Drumset.flac">FLAC</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Drumset.mp3">MP3</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Drumset.ogg">OGG</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Drumset.mid">MIDI</a>,
+        <a href="https://files.penbuvt.ca/music/make-it-through/Penbu%20-%20Make%20It%20Through-Drumset.mxl">MusicXML (MXL)</a>
+      </li>
+    </ul>
+  </li>
+</ul>
+
+<p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><small><a property="dct:title" rel="cc:attributionURL" href="https://www.penbuvt.ca/portfolio/make-it-through/">Make It Through</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://www.penbuvt.ca/">Penbu</a> is licensed under <a href="http://creativecommons.org/licenses/by/4.0/" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY 4.0<img style="height:1em!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"><img style="height:1em!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg"></a></small></p>
