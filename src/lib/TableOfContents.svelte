bad9fed5a1bc13d3276b54fbc1dcf57ec3fabe73
Penbu <penbu@penbuvt.ca>
2025-01-15 22:26:56 -0500

    Add rudimentary table of contents

---
+++ src/lib/TableOfContents.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let headings = {};
+
+  $: orderedHeadings = Object.values(headings ?? {});
+</script>
+
+{#if orderedHeadings.length}
+  <div class="toc">
+    <ol>
+    {#each orderedHeadings as heading (heading.id)}
+      <li><a href="#{heading.id}">{heading.text}</a></li>
+    {/each}
+    </ol>
+  </div>
+{/if}
--- src/routes/(footer)/(header)/+layout.svelte
+++ src/routes/(footer)/(header)/+layout.svelte
@@ -1,14 +1,41 @@
-<script>
+<script lang="ts">
   import NavHeader from '$lib/NavHeader.svelte';
+  import TableOfContents from '$lib/TableOfContents.svelte';
+
+  import { page } from '$app/stores';
 </script>
 
 <NavHeader />
-<div class="content">
-  <slot />
+
+<div class="content-container">
+  <div class="toc-container">
+    <TableOfContents headings={$page.data?.headings} />
+  </div>
+  <div class="content">
+    <slot />
+  </div>
 </div>
 
 <style lang="scss">
+  .content-container {
+    display: flex;
+
+    & > * {
+      flex: 1 1 auto;
+    }
+  }
+
   .content {
     padding: 1em 2em;
+    overflow-x: hidden;
+  }
+
+  .toc-container {
+    flex: 0 1 auto;
+
+    & > :global(.toc) {
+      position: sticky;
+      top: 1rem;
+    }
   }
 </style>
