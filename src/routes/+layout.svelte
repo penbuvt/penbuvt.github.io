027242de81a54234d9386a86e1686af14cf06ff2
Penbu <penbu@penbuvt.ca>
2026-03-31 21:24:10 -0400

    Use Tango colours for links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,6 +27,23 @@
     margin: 0;
   }
 
+  :global(a:link) {
+    color: var(--primary-link-color);
+  }
+
+  :global(a:visited) {
+    color: var(--visited-link-color);
+  }
+
+  :global(a:hover),
+  :global(a:focus) {
+    color: var(--hover-link-color);
+  }
+
+  :global(a:active) {
+    color: var(--active-link-color);
+  }
+
   :root {
     /* Primary theme color */
     --primary-color: #498E4E;
@@ -35,6 +52,9 @@
     --primary-text-color-over: #000;
     /* Primary theme link color */
     --primary-link-color: #204A87;
+    --visited-link-color: #5C3566;
+    --hover-link-color: #3465A4;
+    --active-link-color: #CC0000;
     /* Secondary color: the background body color */
     --secondary-color: #EEEEEC;
     --secondary-text-color: #2E3436;
