3b39a519844f0de4f0bed4112efaf946af82d8c6
Penbu <penbu@penbuvt.ca>
2026-06-11 21:41:31 -0400

    Switch back to vitePreprocess
    Newer versions of SvelteKit and related packages no longer require
    sveltePreprocess for SASS support.
    
    Also switch to sass-embedded for faster processing.

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
 import { mdsvex } from 'mdsvex';
 import toml from 'toml';
 import adapter from '@sveltejs/adapter-auto';
-import sveltePreprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
-		sveltePreprocess(),
+		vitePreprocess(),
 		mdsvex({
 			extensions: ['.svx', '.md'],
 			frontmatter: {
