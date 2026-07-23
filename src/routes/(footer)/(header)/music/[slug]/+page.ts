2362f755bbb95a4e3a953ca6f0d0e6c6e60b50dd
Penbu <penbu@penbuvt.ca>
2026-07-22 21:13:36 -0400

    Handle non-existent pages under /music/

--- src/routes/(footer)/(header)/music/[slug]/+page.ts
+++ src/routes/(footer)/(header)/music/[slug]/+page.ts
@@ -1,9 +1,17 @@
 import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ params }) => {
   const { slug } = params;
   const modules = import.meta.glob('../pages/*.svx');
-  const { default: page, metadata } = await modules[`../pages/${slug}.svx`]?.();
+  const module = await modules[`../pages/${slug}.svx`]?.();
+
+  if (!module) {
+    error(404, 'Not Found');
+    return;
+  }
+
+  const { default: page, metadata } = module;
 
   return {
     title: metadata.title,
